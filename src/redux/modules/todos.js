ddef4a87aeb9bfb0332b211214a83b9503ca5d7d
jennywoon <cjaeks0394@naver.com>
2022-08-01 17:03:28 +0900

    리덕스 todos 모듈 추가

--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -1,11 +1,46 @@
 // src/redux/modules/counter.js
 // Action Value
 
+const ADD_TODO = "ADD_TODO"
+const DELETE_TODO = "DELETO_TODO"
+const DONE_TODO = "DONE_TODO"
 
 // Action Creator
 
+export const addTodo = (todo) => {
+    return {type: ADD_TODO, todo}
+}
+
+export const deleteTodo = (id) => {
+    return {type: DELETE_TODO, id}
+}
+
+export const doneTodo = (id) => {
+    return {type: DONE_TODO, id}
+}
+
 // Initial State
+
+const initialState = [];
 
 // Reducer
 
+const todos = (state = initialState.action) => {
+    switch(action.type){
+        case ADD_TODO:
+            return [...state, action.todo]
+        case DELETE_TODO:
+            const completedTodo = state.filter((todo) => todo.id !== action.id)
+            return completedTodo
+        case DONE_TODO:
+            const doneTodo = state.map((todo) => 
+            todo.id === action.id ? {...todo, isDone: !todo.isDone} : todo)
+            return doneTodo;
+        default:
+            return state;
+    }
+}
+
 // export default reducer
+
+export default todos;
